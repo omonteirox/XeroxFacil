4d35674d873a87aacb2044647453dd3e1c52db63
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-09-21 20:13:29 -0300

    Criando Extensions

---
+++ xeroxFacil/Extensions/ErrosValidatorsExtension.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using xeroxFacil.Models;
+
+namespace xeroxFacil.Extensions
+{
+    public static class ErrosValidatorsExtension
+    {
+        public static string GetError<T>(this IValidator<T> validator, T model)
+        {
+            var errors = new List<string>();
+            foreach (var error in validator.Validate(model).Errors)
+            {
+                return error.ErrorMessage;
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ xeroxFacil/Extensions/ModelStateExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace xeroxFacil.Extensions
+{
+    public static class ModelStateExtension
+    {
+        public static List<string> GetErrors(this ModelStateDictionary modelState)
+        {
+            return modelState.SelectMany(x => x.Value.Errors)
+                .Select(x => x.ErrorMessage)
+                .ToList();
+        }
+    }
+}
